fa2e17d747da06b863dce5da660a9d491ce5538a
CrepHidery <ryan110413@outlook.com>
2026-02-22 10:51:24 +0800

    Update

--- Models/main_module.py
+++ Models/main_module.py
@@ -1,10 +1,9 @@
 import Models.Parser.ActionTable
 import Models.Parser.GrammarTable
-import Models.Parser.DefParser
+from Models.Parser.DefParser import DefinitionType, MoodType, TenseType, OtherType
 import Models.Parser.DicTable
 class SalinModel:
     def __init__(self):
         self.action_table = Models.Parser.ActionTable.ActionTable()
         self.grammar_table = Models.Parser.GrammarTable.GrammarTable()
         self.dic_table = Models.Parser.DicTable.DicTable()
-    
